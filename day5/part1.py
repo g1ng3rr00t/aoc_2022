f4055e63e70c4920a70235c0173cd269ebf09725
g1ng3rr00t <root@ginger.computer>
2022-12-05 09:34:50 -0400

    day5

---
+++ day5/part1.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+main = []
+indices = [1,5,9,13,17,21,25,29,33]
+#indices = [1,5,9]
+#for i in range(3):
+for i in range(9):
+    main.append([])
+
+with open('input','r') as data:
+    for line in data:
+        if 'move' not in line and len(line) > 4:
+            for i in indices:
+                if line[i].isalnum() == True:
+                    main[indices.index(i)].append(line[i])
+                else:
+                    pass
+for i in main:
+    i.reverse()
+
+#print(i)
+
+
+with open('input','r') as data:
+    for line in data:
+        if 'move' in line:
+            commands = line.strip().split(' ')
+            amount = int(commands[1])
+            start = int(commands[3]) -1
+            end = int(commands[5]) - 1
+
+            for i in range(amount):
+                main[end].append(main[start].pop())
+
+for i in main:
+    print(i)
